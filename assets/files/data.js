d90f0b7663599ef9b158d73b0b8a5a209165f50c
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2025-04-22 23:14:13 +0200

    feat: Enhance music command to create voice channel if none exists

--- assets/files/data.js
+++ assets/files/data.js
@@ -1,8 +1,10 @@
+
+
 music = [
-	"https://open.spotify.com/intl-fr/track/3w2EwU9DxU1nNkyAvscRzO?si=f00574126b844b19",
-	"https://open.spotify.com/intl-fr/track/0UFJLWij7K8AXM5sVDFCLj?si=acdb7abb29e345e3",
-	"https://open.spotify.com/intl-fr/track/0trQzsapWGkRe5flRAeXhH?si=d070041dc1c846a8",
-	"https://open.spotify.com/intl-fr/track/13iGfq8apYbk8XPq53ttkm?si=5a770d1becef4e9f",
+	"https://open.spotify.com/intl-fr/track/3w2EwU9DxU1nNkyAvscRzO?si=f00574126b844b19", //DDb de Branlito
+	"https://open.spotify.com/intl-fr/track/0UFJLWij7K8AXM5sVDFCLj?si=acdb7abb29e345e3", //Tratra
+	"https://open.spotify.com/intl-fr/track/0trQzsapWGkRe5flRAeXhH?si=d070041dc1c846a8", //Le sens de la vie de Tal
+	"https://open.spotify.com/intl-fr/track/6FBvXE1VtlEfhqBFZNrJi5?si=66ce238f537c4da1", //Lèche Vitrine d'Adèle Castillon
 ];
 
 idToPromote = ["378634441503014913"];
--- commands/music.js
+++ commands/music.js
@@ -1,14 +1,51 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const axios = require("axios");
-
-let music = require("../assets/files/data").music;
+const { ChannelType } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("music")
 		.setDescription("Joue de la musique"),
 	async execute(interaction) {
-		const randomMusic = music[Math.floor(Math.random() * music.length)];
-		await interaction.reply(randomMusic);
+		const voiceChannel = interaction.member.voice.channel;
+
+		if (!voiceChannel) {
+			const existingChannel = interaction.guild.channels.cache.find(
+				(channel) =>
+					channel.name === "musique" && channel.type === ChannelType.GuildVoice
+			);
+
+			if (!existingChannel) {
+				// Trouver la catégorie "Salons vocaux"
+				const voiceCategory = interaction.guild.channels.cache.find(
+					(channel) =>
+						channel.name === "Salons vocaux" &&
+						channel.type === ChannelType.GuildCategory
+				);
+
+				const newChannel = await interaction.guild.channels.create({
+					name: "musique",
+					type: ChannelType.GuildVoice,
+					parent: voiceCategory?.id || null,
+					userLimit: 10,
+				});
+			}
+			await interaction.reply({
+				content:
+					"Veuillez rejoindre le salon vocal 'musique' pour jouer de la musique.",
+				ephemeral: true,
+			});
+
+			return;
+		} else {
+			//Le user est dans un salon vocal, move le bot dans le salon vocal ou est le user
+			const botChannel = interaction.guild.members.me.voice.channel;
+			if (botChannel) {
+				if (botChannel.id !== voiceChannel.id) {
+					await interaction.member.voice.setChannel(botChannel);
+				}
+			} else {
+				await interaction.member.voice.setChannel(voiceChannel);
+			}
+		}
 	},
 };
